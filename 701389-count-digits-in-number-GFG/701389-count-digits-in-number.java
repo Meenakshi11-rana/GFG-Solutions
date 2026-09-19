7e81c14e40bacf7469d2a13fb0863188a5f87ded
Meenakshi <m54166078@gmail.com>
2026-09-19 21:28:32 +0530

    Force committed solution - Time: O(log n), Space: O(1)

---
+++ 701389-count-digits-in-number-GFG/701389-count-digits-in-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int countDigits(int n) {
+        int count = 0;
+
+        while (n > 0) {
+            count++;
+            n = n / 10;
+        }
+
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
