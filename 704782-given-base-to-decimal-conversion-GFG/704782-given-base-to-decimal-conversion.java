74dcb8f4b524b8ea2dad9b0c8b15da7fe6a4c9df
Meenakshi <m54166078@gmail.com>
2026-09-19 21:33:33 +0530

    Force committed solution - Time: O(N), Space: O(1)

---
+++ 704782-given-base-to-decimal-conversion-GFG/704782-given-base-to-decimal-conversion.java
@@ -0,0 +1,25 @@
+class Solution {
+    static int decimalEquivalent(String n, int b) {
+        int result = 0;
+
+        for (int i = 0; i < n.length(); i++) {
+            char ch = n.charAt(i);
+
+            int digit;
+
+            if (ch >= '0' && ch <= '9') {
+                digit = ch - '0';
+            } else {
+                digit = ch - 'A' + 10;
+            }
+
+            result = result * b + digit;
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
