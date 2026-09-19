252bf609451d1bed9b0063f7c491dc3992a8c0c7
Meenakshi <m54166078@gmail.com>
2026-09-19 21:35:08 +0530

    Force committed solution - Time: O(min(r, n - r)), Space: O(1)

---
+++ 704542-n-cr-GFG/704542-n-cr.java
@@ -0,0 +1,21 @@
+class Solution {
+    static int nCr(int n, int r) {
+        if (r > n) {
+            return 0;
+        }
+
+        r = Math.min(r, n - r);
+
+        long result = 1;
+
+        for (int i = 1; i <= r; i++) {
+            result = result * (n - i + 1) / i;
+        }
+
+        return (int) result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
