a5a14c777db3b79692c77c2919783dcbad20c1b0
Meenakshi <m54166078@gmail.com>
2026-09-19 21:40:09 +0530

    Force committed solution - Time: O(r), Space: O(1)

---
+++ 705035-n-pr-GFG/705035-n-pr.java
@@ -0,0 +1,19 @@
+class Solution {
+    static long nPr(int n, int r) {
+        if (r > n) {
+            return 0;
+        }
+
+        long result = 1;
+
+        for (int i = 0; i < r; i++) {
+            result *= (n - i);
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
