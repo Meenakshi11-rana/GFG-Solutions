46a6f70fffc1f472681ed6a959d905acf177785b
Meenakshi <m54166078@gmail.com>
2026-09-21 01:30:10 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 8015-java-regex-Hackerrank/8015-java-regex.java
@@ -0,0 +1,11 @@
+
+class MyRegex {
+    String pattern =
+        "((25[0-5]|2[0-4][0-9]|1[0-9]{2}|0[0-9]{2}|[0-9]{1,2})\\.){3}" +
+        "(25[0-5]|2[0-4][0-9]|1[0-9]{2}|0[0-9]{2}|[0-9]{1,2})";
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
